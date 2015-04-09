31b02ec2ecbbb39f97c7d826abed46d1a43d7690
Remilia-Scarlet <russel@sgfc.co>
2015-04-09 21:29:04 +0800

    Add a hackfest so the GUI actually remembers our settings.

--- js/Visualiser.js
+++ js/Visualiser.js
@@ -40,14 +40,37 @@ var Visualiser = function ()
 
 Stars.initStars();
 Visualiser.init();
+
+function extend()
+{
+    for (var i = 1; i < arguments.length; i++)
+        for (var key in arguments[i])
+            if (arguments[i].hasOwnProperty(key))
+                arguments[0][key] = arguments[i][key];
+    return arguments[0];
+}
+
+Config = extend(Config, localStorage) || Config;
+Config.gain = parseInt(Math.pow(localStorage["gain"], 0.5) * 100) || 100;
+Config.limitFPS = JSON.parse(Config.limitFPS);
+Config.debug = JSON.parse(Config.debug);
+
 var gui = new dat.GUI();
 gui.closed = true;
-gui.add(Config, 'limitFPS').name("Limit FPS");
-gui.add(Config, 'FPS', 1, 144).name("Maximum FPS");
-gui.add(Config, 'debug').name("Show Debug UI");
-Config.gain = 100;
-gui.add(Config, 'gain', 0, 100).name("Volume").onChange(function(value){
-    gainNode.gain.value = Math.pow(parseInt(value) / parseInt(100), 2);
+gui.add(Config, 'limitFPS').name("Limit FPS").onChange(function (value)
+{
+    localStorage['limitFPS'] = value;
 });
-gui.remember(Config);
-gui.UseLocalStorage = true;
+gui.add(Config, 'FPS', 1, 144).name("Maximum FPS").onChange(function (value)
+{
+    localStorage['FPS'] = value;
+});
+gui.add(Config, 'debug').name("Show Debug UI").onChange(function (value)
+{
+    localStorage['debug'] = value;
+});
+gui.add(Config, 'gain', 0, 100).name("Volume").onChange(function (value)
+{
+    gainNode.gain.value = Math.pow(parseInt(value) / parseInt(100), 2);
+    localStorage["gain"] = gainNode.gain.value;
+});
